f5b3ddc83677234c30cb36639e9ee6c0aff05ee4
Shamil Ramazanov <shamil.ramazanov.1994@mail.ru>
2022-09-19 10:39:12 +0300

    Управление двигателем постоянного тока при помощи релейного модуля

---
+++ relay_motor_control.py
@@ -0,0 +1,20 @@
+'''Управление двигателем постоянного тока при помощи релейного модуля.'''
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+control_pin = 18
+GPIO.setup(control_pin, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(control_pin, False)
+        time.sleep(5)
+        GPIO.output(control_pin, True)
+        time.sleep(2)
+finally:
+    print('Сброс')
+    GPIO.cleanup()
